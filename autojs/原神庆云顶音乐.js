b8d27e70a2dcbdfc3d0c8e5a13a7e24c05221717
TheAwesomeWind <99630383+AwesomeWind@users.noreply.github.com>
2022-02-18 11:55:14 +0800

    Add files via upload

---
+++ autojs/原神庆云顶音乐.js
@@ -0,0 +1,19 @@
+setScreenMetrics(device.width, device.height);
+
+function A1() { press(766, 200, 1) }
+function A2() { press(973, 200, 1) }
+function A3() { press(1170, 200, 1) }
+function A4() { press(1376, 200, 1) }
+function A5() { press(1564, 200, 1) }
+function A6() { press(766, 395, 1) }
+function A7() { press(973, 395, 1) }
+function B1() { press(1170, 396, 1) }
+function B2() { press(1376, 395, 1) }
+function B3() { press(1564, 395, 1) }
+function B4() { press(766, 590, 1) }
+function B5() { press(973, 590, 1) }
+function B6() { press(1170, 590, 1) }
+function B7() { press(1376, 590, 1) }
+function C1() { press(1564, 590, 1) }
+log('开始运行');
+B1();sleep(495);B2();sleep(495);B1();sleep(495);A1();A5();B5();sleep(495);B3();sleep(1485);A6();sleep(495);A5();sleep(495);B1();sleep(495);B2();sleep(495);A3();A6();B6();sleep(495);B3();sleep(495);A2();sleep(495);B1();sleep(495);B2();sleep(495);B1();sleep(495);A4();B5();sleep(495);A1();B3();sleep(495);A5();A6();sleep(495);B2();sleep(495);B1();sleep(495);A4();B1();B5();sleep(495);A1();B3();sleep(495);A4();sleep(495);B2();B3();sleep(495);B2();sleep(495);B1();sleep(495);A1();B5();sleep(495);A5();sleep(495);A2();sleep(495);A3();A5();sleep(1485);A7();sleep(990);B1();sleep(495);B2();sleep(495);B1();sleep(495);A1();B5();sleep(495);A1();B3();sleep(495);A2();sleep(495);A3();sleep(990);A5();sleep(495);A1();sleep(495);A2();sleep(495);A3();sleep(495);A1();A5();sleep(495);B1();sleep(495);B2();sleep(495);B1();sleep(495);A1();B6();sleep(495);A1();B3();sleep(495);A1();sleep(495);A1();sleep(495);A3();sleep(495);A6();sleep(495);A7();sleep(990);A1();sleep(495);A3();A6();sleep(2475);B1();sleep(495);B2();sleep(495);B1();sleep(495);A1();B5();sleep(495);A1();B3();sleep(495);A5();sleep(495);A1();A3();A6();sleep(495);B2();sleep(495);B1();sleep(990);A1();B1();B5();sleep(495);A1();B3();sleep(495);A5();sleep(495);A1();sleep(495);B2();sleep(495);B1();sleep(495);A1();B1();sleep(495);A1();sleep(495);A2();sleep(495);A3();A5();sleep(1980);A7();sleep(1485);B1();sleep(495);B2();sleep(495);B1();sleep(495);A1();A5();B5();sleep(495);B3();sleep(495);B1();sleep(495);B2();sleep(495);B1();sleep(990);A6();B5();sleep(990);B3();sleep(990);B1();sleep(495);B2();sleep(495);B1();sleep(495);A3();A6();B6();sleep(495);B3();sleep(1485);B1();
---
+++ autojs/小鸠 即将到来的明天 演奏版 Kobato 烛子ikina.js
@@ -0,0 +1,19 @@
+setScreenMetrics(device.width, device.height);
+
+function A1() { press(766, 200, 1) }
+function A2() { press(973, 200, 1) }
+function A3() { press(1170, 200, 1) }
+function A4() { press(1376, 200, 1) }
+function A5() { press(1564, 200, 1) }
+function A6() { press(766, 395, 1) }
+function A7() { press(973, 395, 1) }
+function B1() { press(1170, 396, 1) }
+function B2() { press(1376, 395, 1) }
+function B3() { press(1564, 395, 1) }
+function B4() { press(766, 590, 1) }
+function B5() { press(973, 590, 1) }
+function B6() { press(1170, 590, 1) }
+function B7() { press(1376, 590, 1) }
+function C1() { press(1564, 590, 1) }
+log('开始运行');
+A1();B5();sleep(544);B4();sleep(272);B3();sleep(272);B2();sleep(272);B1();sleep(272);A4();B2();sleep(544);B3();sleep(544);B1();sleep(544);A2();A5();sleep(544);B1();sleep(544);A7();sleep(544);A3();B1();sleep(1632);A1();B1();sleep(1088);A5();sleep(544);A6();B3();sleep(1088);B1();sleep(544);A4();B2();sleep(1088);B3();sleep(544);A5();B2();sleep(1632);A1();B3();sleep(544);B4();sleep(544);B5();sleep(544);A4();B6();sleep(544);B5();sleep(544);B4();sleep(544);A2();B3();sleep(1088);B1();sleep(544);A3();B2();sleep(1632);A6();B1();sleep(1088);A5();sleep(544);A4();B3();sleep(1088);B1();sleep(544);A2();B2();sleep(1088);B3();sleep(544);A3();B2();sleep(1632);A6();B3();sleep(544);B4();sleep(544);B5();sleep(544);A4();B6();sleep(544);B5();sleep(544);B4();sleep(544);A5();B3();sleep(1088);B2();sleep(544);A1();B1();sleep(1632);A4();B2();sleep(1088);B3();sleep(544);A2();B4();sleep(1088);B2();sleep(544);A6();B3();sleep(1088);B4();sleep(544);A3();B5();sleep(1632);A2();B6();sleep(544);B5();sleep(544);B4();sleep(544);A6();B3();sleep(544);B2();sleep(544);B1();sleep(544);B1();B2();sleep(1088);B3();sleep(544);A7();B2();sleep(1632);A4();B1();sleep(1088);A5();sleep(544);A5();B3();sleep(1088);B1();sleep(544);A6();B2();sleep(1088);B3();sleep(544);A7();B2();sleep(1632);B1();B3();sleep(544);B4();sleep(544);B5();sleep(544);A4();B6();sleep(544);B5();sleep(544);B4();sleep(544);A5();B3();sleep(1088);B2();sleep(544);A1();B1();sleep(2720);B3();sleep(544);A7();B3();sleep(1088);B7();sleep(544);A5();B6();sleep(1088);B5();sleep(544);A5();B2();sleep(1088);B6();sleep(544);A2();B5();sleep(1088);B3();sleep(544);A1();A7();sleep(544);A6();sleep(816);B3();sleep(272);A2();B3();sleep(544);B2();sleep(544);A7();sleep(272);A7();sleep(272);A3();A5();sleep(544);A3();sleep(544);A7();sleep(544);A3();A6();sleep(1088);A5();sleep(544);A4();A6();sleep(544);A7();sleep(544);B1();sleep(544);A3();B6();sleep(544);B5();sleep(1088);A3();A5();sleep(544);A6();sleep(544);A7();sleep(544);A6();B6();sleep(544);B5();sleep(816);B3();sleep(272);A2();A6();sleep(544);A5();sleep(816);B5();sleep(272);A3();A6();sleep(544);A5();sleep(816);B5();sleep(272);A6();B6();
