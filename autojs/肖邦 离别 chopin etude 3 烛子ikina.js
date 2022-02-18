66899c7853e094d04be320962a09b7bde19cf45c
TheAwesomeWind <99630383+AwesomeWind@users.noreply.github.com>
2022-02-18 12:27:02 +0800

    Add files via upload

---
+++ autojs/肖邦 离别 chopin etude 3 烛子ikina.js
@@ -0,0 +1,24 @@
+//作者AwesomeWind
+//版权归属theAwesomWind
+//！！请勿商用！！
+
+setScreenMetrics(device.width, device.height);
+
+function A1() { press(766, 200, 1) }
+function A2() { press(973, 200, 1) }
+function A3() { press(1170, 200, 1) }
+function A4() { press(1376, 200, 1) }
+function A5() { press(1564, 200, 1) }
+function A6() { press(766, 395, 1) }
+function A7() { press(973, 395, 1) }
+function B1() { press(1170, 396, 1) }
+function B2() { press(1376, 395, 1) }
+function B3() { press(1564, 395, 1) }
+function B4() { press(766, 590, 1) }
+function B5() { press(973, 590, 1) }
+function B6() { press(1170, 590, 1) }
+function B7() { press(1376, 590, 1) }
+function C1() { press(1564, 590, 1) }
+
+log('开始运行');
+sleep(3000);A5();sleep(1000);A1();A3();B1();sleep(500);A5();sleep(500);A3();A7();sleep(500);A5();B1();sleep(500);A4();A7();B2();sleep(500);A5();sleep(500);A4();sleep(500);A5();sleep(500);A4();sleep(500);A5();B3();sleep(500);A4();A7();B3();sleep(500);A5();B2();sleep(500);A1();A3();B1();B3();sleep(500);A5();sleep(500);A3();sleep(500);A5();sleep(500);A1();A3();sleep(500);A5();B1();B4();sleep(500);A5();B4();sleep(500);A5();B1();B3();sleep(500);A5();A7();B6();sleep(500);A5();sleep(500);A5();sleep(500);A5();A7();B5();sleep(500);A1();A5();B4();sleep(500);A5();B1();B3();sleep(500);A3();A7();sleep(500);A5();B1();sleep(500);A4();A6();B2();sleep(500);A5();A7();sleep(500);A4();A6();sleep(500);A5();A7();sleep(500);A4();A6();sleep(500);A5();A7();B3();sleep(500);A4();A6();B3();sleep(500);A5();A7();B2();sleep(500);A1();A3();B1();sleep(500);A5();sleep(500);A3();sleep(500);A5();sleep(500);A1();B3();sleep(500);B1();B4();sleep(500);B2();sleep(500);B1();B3();sleep(500);A4();A6();B4();sleep(500);B1();B5();sleep(500);A6();B3();sleep(500);B1();B4();sleep(500);A4();A6();B6();sleep(500);B1();sleep(500);A6();B2();sleep(500);B1();sleep(500);A5();sleep(500);B1();sleep(500);A5();B4();sleep(250);B3();sleep(250);B1();B2();sleep(500);A4();A6();sleep(500);B1();sleep(500);A6();B4();sleep(250);B3();sleep(250);B1();B2();sleep(500);A4();A7();B5();sleep(500);A5();sleep(500);A4();sleep(500);A5();sleep(500);A1();A3();B1();B3();sleep(500);A5();sleep(500);A3();A7();sleep(500);A5();B1();sleep(500);A4();A7();B2();sleep(500);A5();sleep(500);A4();sleep(500);A5();sleep(500);A4();sleep(500);A5();B3();sleep(500);A4();A7();B3();sleep(500);A5();B2();sleep(500);A1();A3();B1();B3();sleep(500);A5();sleep(500);A3();sleep(500);A5();sleep(500);A1();A3();sleep(500);A5();B1();B4();sleep(500);A5();B4();sleep(500);A5();B1();B3();sleep(500);A5();A7();B6();sleep(500);A5();sleep(500);A5();sleep(500);A5();A7();B5();sleep(500);A1();A5();B4();sleep(500);A5();B1();B3();sleep(500);A3();A7();sleep(500);A5();B1();sleep(500);A4();A6();B2();sleep(500);A5();A7();sleep(500);A4();A6();sleep(500);A5();A7();sleep(500);A4();A6();sleep(500);A5();A7();B3();sleep(500);A4();A6();B3();sleep(500);A5();A7();B2();sleep(500);A1();A3();B1();sleep(500);A5();sleep(500);A3();sleep(500);A5();sleep(500);A1();B2();B5();sleep(500);B1();B3();B6();sleep(500);B2();B6();sleep(500);B1();B3();B5();sleep(500);A4();A6();B4();sleep(500);B1();B5();sleep(500);A6();B3();sleep(500);B1();B4();sleep(500);A3();B2();B7();sleep(500);B3();C1();sleep(500);B2();C1();sleep(500);B3();B7();sleep(500);A6();B1();B6();sleep(500);B3();B7();sleep(500);B1();B7();sleep(500);B3();B6();sleep(500);A2();A6();B1();sleep(500);A2();A6();B2();sleep(500);A2();A6();A7();sleep(500);A2();A6();B1();sleep(500);A2();A4();A6();B2();sleep(500);A3();A4();A6();B3();sleep(500);A1();A4();A6();B1();sleep(500);A2();A4();A6();B2();sleep(500);A1();A3();B1();B3();sleep(500);A5();sleep(500);A3();sleep(500);A5();sleep(500);A3();sleep(500);A5();B2();sleep(500);A3();B1();sleep(500);A5();A6();sleep(500);A2();A7();sleep(500);A5();sleep(500);A7();sleep(500);B2();sleep(500);A6();B1();C1();sleep(500);B3();B7();sleep(500);A3();B1();B6();sleep(500);B3();sleep(500);A5();A7();B5();sleep(500);A7();sleep(500);A3();A5();sleep(500);A7();sleep(500);A4();A6();B6();sleep(500);B1();B5();sleep(500);A1();A6();B4();sleep(500);B1();sleep(500);A3();A5();B3();sleep(500);A5();sleep(500);A1();A3();sleep(500);A5();sleep(500);A3();sleep(500);A5();sleep(500);A1();A3();sleep(500);A5();sleep(500);A3();B1();B3();sleep(250);B2();sleep(250);B1();
